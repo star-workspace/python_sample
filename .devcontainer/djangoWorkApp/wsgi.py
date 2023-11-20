233f2e886954a4c03c184ad0af5257dfb4d44458
DESKTOP-78S4HMK\User <ghost2child@gmail.com>
2023-11-20 23:54:14 +0900

    Djangoの初期ファイルを追加

---
+++ .devcontainer/djangoWorkApp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djangoWorkApp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoWorkApp.settings')
+
+application = get_wsgi_application()
